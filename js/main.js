c2ea6ee4014d3e3c0e4219d75671b45aadf3e851
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2020-08-27 11:24:27 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function(){
+
+    let heightBg = $('.header__bg-image').width() / 2,
+        heightForHeader = (heightBg - $(window).height()) + $(window).height();
+    
+        if(heightForHeader >= 800) {
+            $('.header').css('height', heightForHeader + 'px');
+        }
+
+    let image_srcNotWebp,
+        image_src_bg = '.webp-bg';
+
+    function ThisIsWebP() {
+        let def = $.Deferred(), crimg = new Image();
+        crimg.onload = function () { def.resolve(); };
+        crimg.onerror = function () { def.reject(); };
+        crimg.src = "https://simpl.info/webp/cherry.webp";
+        return def.promise();
+    }
+
+    ThisIsWebP().then(function () {
+        $.each($(image_src_bg), function () {
+            return false;
+        });
+    }, function () {
+            $.each($(image_src_bg), function () {
+                image_srcNotWebp = $(this).data('notwebp');
+                $(this).css('background-image', 'url("' + image_srcNotWebp + '")');
+            });
+        });
+
+        function customMediaEvents() {
+            
+        }
+
+        $(window).resize(function() {
+            customMediaEvents();
+            heightBg = $('.header__bg-image').width() / 2,
+            heightForHeader = (heightBg - $(window).height()) + $(window).height();
+            if(heightForHeader >= 800) {
+                $('.header').css('height', heightForHeader + 'px');
+            }
+            
+        });
+
+        AOS.init();
+
+});
